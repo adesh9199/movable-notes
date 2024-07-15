a16579569fb36e507e4f054763aa2ac6897d1a68
Adesh  Kumar Dubey <104526444+adesh9199@users.noreply.github.com>
2024-07-15 17:26:12 +0530

    Add files via upload

---
+++ src/components/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Background() {
+  return (
+    <>
+    <dir className='fixed z-[2] w-full h-screen'>
+
+       <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[10vw] leading-none tracking-tighter font-semibold text-zinc-900'>PassWizard</h1>
+    </dir>
+    
+    </>
+  )
+}
+
+export default Background
